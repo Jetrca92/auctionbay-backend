a757bc3ceb87128c85c9b87c02fc8e28011d3e31
Jetrca92 <blaz.guzej@gmail.com>
2024-09-05 11:11:08 +0200

    Prepare initial structure

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './modules/app.module'
+import { Logger } from '@nestjs/common'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule)
+  app.enableCors({
+    origin: ['http://localhost:3000'],
+    credentials: true,
+  })
+
+  const PORT = process.env.PORT || 8080
+  await app.listen(PORT)
+
+  Logger.log(`App is listening on http://localhost:${PORT}`)
 }
-bootstrap();
+bootstrap()
