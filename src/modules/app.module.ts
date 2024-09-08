1fd175cbb790cb6cf33a9e2f11a725ac3b72ddfb
Jetrca92 <blaz.guzej@gmail.com>
2024-09-08 19:45:35 +0200

    Set up orm, connect to db

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,7 +1,18 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { configValidationSchema } from '../config/schema.config'
+
+import { DatabaseModule } from './database/database.module'
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: [`.env.${process.env.STAGE}`],
+      validationSchema: configValidationSchema,
+    }),
+    DatabaseModule,
+  ],
   controllers: [],
   providers: [],
 })
