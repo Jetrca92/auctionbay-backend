d68fc0db956d018b337cb096a008a14d6396608f
Jetrca92 <blaz.guzej@gmail.com>
2024-09-15 20:53:19 +0200

    Complete patch auction

--- src/modules/auction/auction.controller.ts
+++ src/modules/auction/auction.controller.ts
@@ -1,18 +1,36 @@
-import { Body, Controller, HttpCode, HttpStatus, Post, UseGuards } from '@nestjs/common'
+import { Body, Controller, Get, HttpCode, HttpStatus, Param, Patch, Post, UseGuards } from '@nestjs/common'
 import { AuctionService } from './auction.service'
 import { AuthGuard } from '@nestjs/passport'
 import { GetCurrentUserById } from 'utils/get-user-by-id.decorator'
 import { Auction } from 'entities/auction.entity'
 import { CreateAuctionDto } from './dto/create-auction.dto'
+import { UpdateAuctionDto } from './dto/update-auction.dto'
 
 @Controller('me/auction')
 export class AuctionController {
   constructor(private readonly auctionService: AuctionService) {}
 
+  @Get()
+  @HttpCode(HttpStatus.OK)
+  async findAuctions(): Promise<Auction[]> {
+    return this.auctionService.findAll()
+  }
+
   @UseGuards(AuthGuard('jwt'))
   @Post()
-  @HttpCode(HttpStatus.OK)
+  @HttpCode(HttpStatus.CREATED)
   async create(@Body() createAuctionDto: CreateAuctionDto, @GetCurrentUserById() userId: string): Promise<Auction> {
     return this.auctionService.create(createAuctionDto, userId)
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Patch(':id')
+  @HttpCode(HttpStatus.OK)
+  async update(
+    @Param('id') auctionId: string,
+    @GetCurrentUserById() userId: string,
+    @Body() updateAuctionDto: UpdateAuctionDto,
+  ): Promise<Auction> {
+    return this.auctionService.update(auctionId, userId, updateAuctionDto)
   }
 }
--- src/modules/auction/auction.service.ts
+++ src/modules/auction/auction.service.ts
@@ -1,10 +1,18 @@
-import { BadRequestException, Injectable, Logger } from '@nestjs/common'
+import {
+  BadRequestException,
+  ForbiddenException,
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+  NotFoundException,
+} from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm'
 import { Auction } from 'entities/auction.entity'
 import { AbstractService } from 'modules/common/abstract.service'
 import { CreateAuctionDto } from 'modules/auction/dto/create-auction.dto'
 import { Repository } from 'typeorm'
 import { User } from 'entities/user.entity'
+import { UpdateAuctionDto } from './dto/update-auction.dto'
 
 @Injectable()
 export class AuctionService extends AbstractService {
@@ -22,11 +30,29 @@ export class AuctionService extends AbstractService {
         throw new BadRequestException('User not found')
       }
       const newAuction = this.auctionRepository.create({ ...createAuctionDto, owner: user })
-      Logger.log(JSON.stringify(newAuction))
+      Logger.log(`Creating new auction ${newAuction.title} by ${userId} user id.`)
       return this.auctionRepository.save(newAuction)
     } catch (error) {
       Logger.log(error)
-      throw new BadRequestException('Something went wrong while creating a new auction.')
+      throw new InternalServerErrorException('Something went wrong while creating a new auction.')
+    }
+  }
+
+  async update(auctionId: string, userId: string, updateAuctionDto: UpdateAuctionDto): Promise<Auction> {
+    const auction = (await this.findById(auctionId, ['owner'])) as Auction
+    if (!auction) throw new NotFoundException(`Auction with ID ${auctionId} not found`)
+    if (auction && auction.owner) {
+      console.log('Owner ID:', auction.owner.id)
+    } else {
+      console.log('Owner not found')
+    }
+    if (auction.owner.id !== userId) throw new ForbiddenException('You are the owner of this auction')
+    try {
+      Object.assign(auction, updateAuctionDto)
+      return await this.auctionRepository.save(auction)
+    } catch (error) {
+      Logger.error(error)
+      throw new InternalServerErrorException('Something went wrong while updating the auction.')
     }
   }
 }
